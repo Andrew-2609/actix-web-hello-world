94a58dd182f06aa8824e0bb493696e048a103981
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-04-19 18:42:00 -0300

    feat: add first hello world routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!\n")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("{req_body}\n"))
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey, there!\n")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
