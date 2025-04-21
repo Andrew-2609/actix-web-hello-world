5b594167d2409f15203db057614f0164d1ec2cd6
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-04-21 15:21:47 -0300

    feat: implement examples with local and global counters

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,54 @@
-use serde::Deserialize;
-use std::{collections::HashMap, sync::Mutex};
+use std::{
+    cell::Cell,
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, Result, error, get, web};
+use actix_web::{App, HttpServer, Responder, get, post, web};
 
+#[derive(Clone)]
 struct AppState {
-    users: Mutex<HashMap<i64, String>>,
+    local_count: Cell<usize>,
+    global_count: Arc<AtomicUsize>,
 }
 
-#[derive(Deserialize)]
-struct User {
-    user_id: Option<i64>,
-    user_name: String,
+#[get("/count")]
+async fn show_count(data: web::Data<AppState>) -> impl Responder {
+    format!(
+        "global_count: {}\nlocal_count: {}\n",
+        data.global_count.load(Ordering::Relaxed),
+        data.local_count.get()
+    )
 }
 
-#[get("/users/{user_id}")]
-async fn index(path: web::Path<i64>, data: web::Data<AppState>) -> impl Responder {
-    let user_id = path.into_inner();
-    let users = data.users.lock().unwrap();
-    let user = users.get(&user_id);
+#[post("/count")]
+async fn increment_count(data: web::Data<AppState>) -> impl Responder {
+    data.global_count.fetch_add(1, Ordering::Relaxed);
 
-    let result: String = user.unwrap_or(&"User not found".to_string()).into();
-    format!("{result}\n")
-}
+    let local_count = data.local_count.get();
+    data.local_count.set(local_count + 1);
 
-async fn add_user(info: web::Json<User>, data: web::Data<AppState>) -> Result<String> {
-    let user_id = info.user_id;
-    let user_name = &info.user_name;
-
-    let mut users = data.users.lock().unwrap();
-    let users_size = i64::try_from(users.len()).unwrap();
-    let user_id: i64 = user_id.unwrap_or(users_size + 1);
-
-    if !users.contains_key(&user_id) {
-        users.insert(user_id, user_name.clone());
-    }
-
-    let user = users.get(&user_id).unwrap();
-
-    Ok(format!("{user}\n"))
+    format!(
+        "global_count: {}\nlocal_count: {}\n",
+        data.global_count.load(Ordering::Relaxed),
+        data.local_count.get()
+    )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_state = web::Data::new(AppState {
-        users: Mutex::new(HashMap::from([(1, String::from("Andrew"))])),
-    });
+    let app_data = AppState {
+        local_count: Cell::new(0),
+        global_count: Arc::new(AtomicUsize::new(0)),
+    };
 
     HttpServer::new(move || {
-        let json_config = web::JsonConfig::default()
-            .limit(4096)
-            .error_handler(|err, _req| {
-                error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
-            });
-
         App::new()
-            .app_data(app_state.clone())
-            .service(index)
-            .service(
-                web::resource("/users")
-                    .app_data(json_config)
-                    .route(web::post().to(add_user)),
-            )
+            .app_data(web::Data::new(app_data.clone()))
+            .service(show_count)
+            .service(increment_count)
     })
     .bind("0.0.0.0:8081")?
     .run()
