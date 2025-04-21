d7bf4c6e3ea8e4b42d6e920d4a4fa73e80816f24
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-04-21 15:04:46 -0300

    feat: implement simple logic for registering and querying users

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,59 @@
-use actix_web::{App, HttpResponse, HttpServer, web};
+use serde::Deserialize;
+use std::{collections::HashMap, sync::Mutex};
 
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("/app\n") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+use actix_web::{App, HttpServer, Responder, Result, get, post, web};
+
+struct AppState {
+    users: Mutex<HashMap<i64, String>>,
 }
 
-fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("/test\n") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+#[derive(Deserialize)]
+struct User {
+    user_id: Option<i64>,
+    user_name: String,
+}
+
+#[get("/users/{user_id}")]
+async fn index(path: web::Path<i64>, data: web::Data<AppState>) -> impl Responder {
+    let user_id = path.into_inner();
+    let users = data.users.lock().unwrap();
+    let user = users.get(&user_id);
+
+    let result: String = user.unwrap_or(&"User not found".to_string()).into();
+    format!("{result}\n")
+}
+
+#[post("/users")]
+async fn add_user(info: web::Json<User>, data: web::Data<AppState>) -> Result<String> {
+    let user_id = info.user_id;
+    let user_name = &info.user_name;
+
+    let mut users = data.users.lock().unwrap();
+    let users_size = i64::try_from(users.len()).unwrap();
+    let user_id: i64 = user_id.unwrap_or(users_size + 1);
+
+    if !users.contains_key(&user_id) {
+        users.insert(user_id, user_name.clone());
+    }
+
+    let user = users.get(&user_id).unwrap();
+
+    Ok(format!("{user}\n"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_state = web::Data::new(AppState {
+        users: Mutex::new(HashMap::from([(1, String::from("Andrew"))])),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .configure(config)
-            .service(web::scope("/api").configure(scoped_config))
-            .route("/", web::to(|| async { HttpResponse::Ok().body("/\n") }))
+            .app_data(app_state.clone())
+            .service(index)
+            .service(add_user)
     })
-    .bind(("127.0.0.1", 8081))?
+    .bind("0.0.0.0:8081")?
     .run()
     .await
 }
