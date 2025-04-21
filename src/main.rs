922dcb8d9c9b10a7c45d9debf2ec559627d3e810
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-04-21 15:28:25 -0300

    feat: implement custom Responder

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,35 @@
-use std::{
-    cell::Cell,
-    sync::{
-        Arc,
-        atomic::{AtomicUsize, Ordering},
-    },
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Responder, body::BoxBody, get,
+    http::header::ContentType,
 };
+use serde::Serialize;
 
-use actix_web::{App, HttpServer, Responder, get, post, web};
-
-#[derive(Clone)]
-struct AppState {
-    local_count: Cell<usize>,
-    global_count: Arc<AtomicUsize>,
+#[derive(Serialize)]
+struct MyObj {
+    name: &'static str,
 }
 
-#[get("/count")]
-async fn show_count(data: web::Data<AppState>) -> impl Responder {
-    format!(
-        "global_count: {}\nlocal_count: {}\n",
-        data.global_count.load(Ordering::Relaxed),
-        data.local_count.get()
-    )
+impl Responder for MyObj {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body + "\n")
+    }
 }
 
-#[post("/count")]
-async fn increment_count(data: web::Data<AppState>) -> impl Responder {
-    data.global_count.fetch_add(1, Ordering::Relaxed);
-
-    let local_count = data.local_count.get();
-    data.local_count.set(local_count + 1);
-
-    format!(
-        "global_count: {}\nlocal_count: {}\n",
-        data.global_count.load(Ordering::Relaxed),
-        data.local_count.get()
-    )
+#[get("/")]
+async fn index() -> impl Responder {
+    MyObj { name: "Andrew" }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_data = AppState {
-        local_count: Cell::new(0),
-        global_count: Arc::new(AtomicUsize::new(0)),
-    };
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(app_data.clone()))
-            .service(show_count)
-            .service(increment_count)
-    })
-    .bind("0.0.0.0:8081")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:8081")?
+        .run()
+        .await
 }
